36f9e7b926348420fedebbb94688bbfc25a65e33
Ananias Barbosa <ananiasgomes27@gmail.com>
2022-09-15 00:42:31 -0300

    chore: fix

--- public/js/climate.js
+++ public/js/climate.js
@@ -6,13 +6,13 @@ const unidade = "metric";
 
 const { longitude } = dadosRegiao;
 const { latitude } = dadosRegiao;
-const { nome } = dadosRegiao;
+const { cidade } = dadosRegiao;
 console.log(latitude);
 console.log(longitude);
 const url = `${BASE_URL}?lat=${latitude}&lon=${longitude}&units=${unidade}&appid=${apiKey}&lang=${lang}`;
 
 document.getElementById("back").style.visibility = "visible";
-document.getElementById("cidade").innerHTML = nome;
+document.getElementById("cidade").innerHTML = cidade;
 fetch(url)
   .then((response) => response.json())
   .then((dadosTemp) => {
@@ -26,8 +26,7 @@ fetch(url)
 function renderTemp(dadosTemp) {
   dadosTemp = dadosTemp.daily[0];
   const temp = dadosTemp.temp;
-  document.getElementById("weather").innerHTML =
-    dadosTemp.weather[0].description;
+  document.getElementById("weather").innerHTML = dadosTemp.weather[0].description;
   document.getElementById("temp").innerHTML = temp.day;
   document.getElementById("max").innerHTML = temp.max;
   document.getElementById("min").innerHTML = temp.min;
