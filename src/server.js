83942e798a2491da70afd99b4617d704d1be8b3d
Ananias Barbosa <ananiasgomes27@gmail.com>
2022-09-14 23:52:45 -0300

    chode: version 1.0.1

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
